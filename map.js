a745892f439c79d738afdf17c88731e3ff1e525e
SuziHadzhieva <suzivenih@gmail.com>
2024-04-18 11:23:01 +0300

    add home button

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
-require(["esri/config", "esri/WebMap", "esri/views/MapView"],
-    function (esriConfig, WebMap, MapView) {
+require(["esri/config", "esri/WebMap", "esri/views/MapView", "esri/widgets/Home"],
+    function (esriConfig, WebMap, MapView, Home) {
 
         esriConfig.apiKey = "AAPK93e15526c55940619631d3b96000d5acRAQLN1zcT72Ln25ryedldXRP9pNurIj0EV-OCJIGsVvMARaHMy9KvnagXey2qa-f";
 
@@ -12,5 +12,11 @@ require(["esri/config", "esri/WebMap", "esri/views/MapView"],
         const view= new MapView({
             container: "viewDiv",
             map: webmap
-        })
+        });
+
+        const homeBtn = new Home({
+            view
+        });
+
+        view.ui.add(homeBtn, "bottom-left");
     })
